5081e3acdcba0d85104796ee7e380b1c54aa493a
pavel.genuine <pavel.play@gmail.com>
2022-07-15 22:23:35 +0600

    update modal added

--- src/Components/Update/Update.js
+++ src/Components/Update/Update.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Form, Checkbox, Button, Modal, Header } from 'semantic-ui-react';
 
 const Update = () => {
     return (
-        <div>
-            
+        <div style={{ width: '80%', margin: 'auto', marginTop: '2%', marginBottom: '2%' }}>
+
+            <Form>
+                <Form.Field>
+                    <label>Name</label>
+                    <input placeholder='Name' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Description</label>
+                    <input placeholder='Description' />
+                </Form.Field>
+
+                <Button primary type='submit'>Submit</Button>
+            </Form>
         </div>
     );
 };
