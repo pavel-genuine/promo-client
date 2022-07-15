cb1bf54960ced54c295162e05a1974f9c5b73336
pavel.genuine <pavel.play@gmail.com>
2022-07-15 22:19:02 +0600

    update & delete options added

--- src/Components/SinglePromo/SinglePromo.js
+++ src/Components/SinglePromo/SinglePromo.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid, Dropdown, Modal, Header, Item, Card, Image, Form, Checkbox, Button, Icon } from 'semantic-ui-react';
+import Update from '../Update/Update';
 
 
 const SinglePromo = () => {
+
+
+    const [open, setOpen] = useState(false)
+
+    const removeHandler = () => {
+        setOpen(false)
+    }
+
+    const handleSubmit = (e) => {
+        console.log(e);
+    }
+
     return (
         <div style={{ width: '60%', margin: 'auto', marginTop: '10%' }}>
 
@@ -13,7 +26,65 @@ const SinglePromo = () => {
                 wrapped ui={false} />
             <Card.Content>
 
-               
+                <Grid  >
+                    <Grid.Row>
+                        <Grid.Column width={14}>
+                            <Card.Header>Matthew</Card.Header>
+                        </Grid.Column>
+                        <Grid.Column width={1}>
+
+                            <Dropdown multiple icon='ellipsis vertical' size='large' >
+
+                                <Dropdown.Menu  >
+                                    <Dropdown.Item icon={<Modal
+                                        as={Form}
+                                        onSubmit={e => handleSubmit(e)}
+                                        trigger={<Button color='primary' basic><Icon name='edit' text='edit' />Edit</Button>}
+                                        header='Update Card!'
+                                        content={<Update></Update>}
+                                    />} />
+
+                                    <Dropdown.Item
+
+                                        icon={
+                                            <Modal
+                                                basic
+                                                onClose={() => setOpen(false)}
+                                                onOpen={() => setOpen(true)}
+                                                open2={open}
+                                                size='small'
+                                                trigger={<Button color='red' basic><Icon name='delete' text='delete' />Delete</Button>}
+                                            >
+                                                <Header icon>
+                                                    <Icon name='archive' />
+                                                    Delete Messages
+                                                </Header>
+                                                <Modal.Content>
+                                                    <p>
+                                                        Wanna Delete ?
+                                                    </p>
+                                                </Modal.Content>
+                                                <Modal.Actions>
+                                                    <Button basic color='red' inverted onClick={() => setOpen(false)}>
+                                                        <Icon name='remove' /> No
+                                                    </Button>
+                                                    <Button color='green' inverted onClick={removeHandler}>
+                                                        <Icon name='checkmark' /> Yes
+                                                    </Button>
+                                                </Modal.Actions>
+                                            </Modal>
+                                        } />
+
+                                </Dropdown.Menu>
+                            </Dropdown>
+
+
+
+                        </Grid.Column>
+
+                    </Grid.Row>
+                </Grid>
+
 
                 <Card.Meta>
                     <span className='date'>Joined in 2015</span>
