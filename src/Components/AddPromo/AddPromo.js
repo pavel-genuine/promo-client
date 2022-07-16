364241fec9a9a46534acfa846e412163039bdbe7
pavel.genuine <pavel.play@gmail.com>
2022-07-16 08:13:53 +0600

    add options

--- src/Components/AddPromo/AddPromo.js
+++ src/Components/AddPromo/AddPromo.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Form, Checkbox, Button, Modal, Header } from 'semantic-ui-react';
+
 
 const AddPromo = () => {
     return (
         <div>
-            
+            <div style={{ width: '50%', margin: 'auto', marginTop: '5%', marginBottom: '2%' }}>
+
+                <Form>
+                    <Form.Field>
+                        <label>Image URL</label>
+                        <input placeholder='Image' />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Name</label>
+                        <input placeholder='Name' />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Description</label>
+                        <input placeholder='Description' />
+                    </Form.Field>
+
+                    <Button primary type='submit'>Submit</Button>
+                </Form>
+            </div>
         </div>
     );
 };
--- src/Components/AllPromos/AllPromos.js
+++ src/Components/AllPromos/AllPromos.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import SinglePromo from '../SinglePromo/SinglePromo';
 
 const AllPromos = () => {
 
-    
+
 
 
     return (
         <div>
-            
+            <SinglePromo></SinglePromo>
         </div>
     );
 };
